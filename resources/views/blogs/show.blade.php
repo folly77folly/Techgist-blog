2ddc87666b41d879b6ac121ab65dae3e3ba72e9a
folly77folly <folly77folly@gmail.com>
2020-07-20 03:02:22 +0100

    added disqus comment feature

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -47,5 +47,17 @@
         <a href="{{route('categories.show', $category->slug) }}"><span>{{ $category->name }}</span></a>
         @endforeach
         </div>
+        <hr>
+        <aside>
+            <div id="disqus_thread"></div>
+                <script>
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://rerte.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+                </script>
+        </aside>
     </div>
 @endsection
