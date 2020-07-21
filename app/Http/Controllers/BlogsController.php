4659c4e8cff74c8a2034599fe0979703fcaee2bb
folly77folly <folly77folly@gmail.com>
2020-07-21 12:53:48 +0100

    added all blogs to index

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -22,8 +22,8 @@ class BlogsController extends Controller
     
     //
     public function index(){
-        // $blogs = Blog::latest()->get();
-        $blogs = Blog::where('status', 1)->latest()->get();
+        $blogs = Blog::latest()->get();
+        // $blogs = Blog::where('status', 1)->latest()->get();
         return view('blogs.index', compact('blogs'));
     }
 
